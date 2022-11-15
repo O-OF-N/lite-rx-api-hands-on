d5dd05ff05b206961aee5405f00b94255ccd8d1a
vmohanan1 <vinodm1986@gmail.com>
2022-11-15 10:19:34 -0500

    Update Part05Merge.java

--- src/main/java/io/pivotal/literx/Part05Merge.java
+++ src/main/java/io/pivotal/literx/Part05Merge.java
@@ -4,6 +4,8 @@ import io.pivotal.literx.domain.User;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.stream.Collectors;
+
 /**
  * Learn how to merge flux.
  *
@@ -15,21 +17,30 @@ public class Part05Merge {
 
 	// TODO Merge flux1 and flux2 values with interleave
 	Flux<User> mergeFluxWithInterleave(Flux<User> flux1, Flux<User> flux2) {
-		return null;
+		System.out.println("flux1 = " + flux1.toStream().collect(Collectors.toList()));
+		System.out.println("flux2 = " + flux2.toStream().collect(Collectors.toList()));
+		Flux<User> flux3=  Flux.merge(flux1,flux2);
+		System.out.println("flux3 = " + flux3.toStream().collect(Collectors.toList()));
+		return flux3;
 	}
 
 //========================================================================================
 
 	// TODO Merge flux1 and flux2 values with no interleave (flux1 values and then flux2 values)
 	Flux<User> mergeFluxWithNoInterleave(Flux<User> flux1, Flux<User> flux2) {
-		return null;
+
+		System.out.println("flux1 = " + flux1.toStream().collect(Collectors.toList()));
+		System.out.println("flux2 = " + flux2.toStream().collect(Collectors.toList()));
+		Flux<User> flux3=  Flux.concat(flux1,flux2);
+		System.out.println("flux3 = " + flux3.toStream().collect(Collectors.toList()));
+		return flux3;
 	}
 
 //========================================================================================
 
 	// TODO Create a Flux containing the value of mono1 then the value of mono2
 	Flux<User> createFluxFromMultipleMono(Mono<User> mono1, Mono<User> mono2) {
-		return null;
+		return Flux.concat(mono1,mono2);
 	}
 
 }
